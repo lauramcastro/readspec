f6e47c84f8a684af1cf6819e2ab17635a96ba383
Laura M. Castro <lcastro@udc.es>
2014-01-16 15:12:27 +0100

    Scenarios can now have more than 1 step.

--- src/readspec.erl
+++ src/readspec.erl
@@ -8,6 +8,7 @@
 %%% @end
 %%% Created :  8 Jan 2014 by Laura M. Castro <lcastro@udc.es>
 %%%-------------------------------------------------------------------
+
 -module(readspec).
 
 -export([suite/3, counterexample/1]).
@@ -30,20 +31,22 @@ suite(Model, Property, NumTests) ->
 	{ok, _CoverFile} = cover:analyse_to_file(Model), %%% [removable up to here]
 %	{ok, _CoverFile} = smother:analyse_to_file(Model), %%% [removable up to here]
 	?DEBUG("Cucumberising set of test cases~n", []),
-	ok = file:write_file("suite.cucumberl", io_lib:fwrite("~p~n", [cucumberise_suite(eqc_suite:cases(Suite))])).
+	ok = file:write_file("suite.cucumberl", io_lib:fwrite("SCENARIO: ~p~n", [cucumberise_suite(eqc_suite:cases(Suite))])).
 
 counterexample(Counterexample) ->
 	cucumberise_teststeps_aux(Counterexample, []).
 
-%%% ------------------------------------------------- %%%
+%%% -------------------------------------------------------------- %%%
 
 cucumberise_suite(Suite) ->
-	cucumberise_testcases_aux(Suite, []).
+	cucumberise_testcases(Suite, []).
 
-cucumberise_testcases_aux([], CucumberisedTestCases) ->
+
+cucumberise_testcases([], CucumberisedTestCases) ->
 	CucumberisedTestCases;
-cucumberise_testcases_aux([TestCase | MoreTestCases], CucumberisedTestCases) ->
-	cucumberise_testcases_aux(MoreTestCases, [cucumberise_teststeps(TestCase) | CucumberisedTestCases]).
+cucumberise_testcases([TestCase | MoreTestCases], CucumberisedTestCases) ->
+	cucumberise_testcases(MoreTestCases, [cucumberise_teststeps(TestCase) | CucumberisedTestCases]).
+
 
 cucumberise_teststeps([TestCase]) ->
 	cucumberise_teststeps_aux(TestCase, []);
@@ -51,7 +54,7 @@ cucumberise_teststeps(TestCase) ->
 	cucumberise_teststeps_aux(TestCase, []).
 
 cucumberise_teststeps_aux([], CucumberisedTestSteps) ->
-	cucumberise({scenario, lists:reverse(CucumberisedTestSteps)});
+	lists:flatten(cucumberise({scenario, lists:reverse(CucumberisedTestSteps)}));
 % tests for QC properties
 cucumberise_teststeps_aux(Values, []) when is_tuple(Values) ->
 	cucumberise({scenario, Values});
@@ -59,17 +62,40 @@ cucumberise_teststeps_aux(Values, []) when is_tuple(Values) ->
 cucumberise_teststeps_aux([{set,_,Call={call,_Module,_Function,_Args}} | MoreSteps], CucumberisedTestSteps) ->
 	cucumberise_teststeps_aux(MoreSteps, [Call | CucumberisedTestSteps]).
 
-% one of the things to do is to remove cases such as {scenario, []}
+
+% we remove spureous cases such as {scenario, []}
 cucumberise({scenario, []}) ->
 	"";
+% cucumberise QC property scenario
+cucumberise({scenario, Values}) when is_tuple(Values) ->
+	explain(Values, []);
+% cucumberise QC state machine scenario
 cucumberise({scenario, [Call={call,_Module,_Function,_Args} | MoreSteps]}) ->
-	explain(Call, MoreSteps);
-cucumberise({scenario, [Values]}) ->
-	"GIVEN " ++ io_lib:fwrite("~p", [Values]) ++
-	" WHEN we need the property body" ++
-    " THEN true".
+	explain(Call, MoreSteps).
 
-explain({call,_Module,Function,Args}, _WeIgnoreOtherSteps) ->
-	"GIVEN " ++ io_lib:fwrite("~p", [Args]) ++
+
+explain({call,_Module,Function,Args}, MoreSteps) ->
+	"GIVEN " ++ enumerate_list([Args]) ++
     " WHEN " ++ io_lib:fwrite("~p", [Function]) ++
-	" THEN " ++ "we need the property postcondition".
+		explain_also(MoreSteps) ++
+	" THEN ** insert property postcondition here ** \n";
+explain(Values, []) ->
+	"GIVEN " ++ enumerate_list(Values) ++
+	" THEN ** insert property body here **".
+
+
+explain_also([]) ->
+	"";
+explain_also([{call,_Module,Function,_ArgsNotUsedRightNow} | MoreSteps]) ->
+	" AND " ++ io_lib:fwrite("~p", [Function]) ++
+		explain_also(MoreSteps).
+
+% ----- ----- ----- ----- ----- -----  ----- ----- ----- ----- ----- %
+
+enumerate_list(Tuple) when is_tuple(Tuple) ->
+	enumerate_list(erlang:tuple_to_list(Tuple));
+enumerate_list(List) when is_list(List) ->
+	L = [io_lib:fwrite("AND ~p ", [X]) || X <- List],
+	[$A, $N, $D, 32 | T ] = lists:flatten(L),
+	T.
+
