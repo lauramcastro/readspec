23df2dcf57306fd88822b2936bd6dfd0c829ff3d
Laura M. Castro <lcastro@udc.es>
2014-02-19 18:06:38 +0100

    Added EDOC comments to sample file.

--- priv/simple_eqc.erl
+++ priv/simple_eqc.erl
@@ -1,13 +1,19 @@
-%%% File    : simple_eqc.erl
-%%% Description : Simple QuickCheck properties
-%%% Created : Jan 2014
-
+%%% -*- coding: utf-8 -*-
+%%% @author Laura M. Castro <lcastro@udc.es>
+%%% @copyright (C) 2014
+%%% @doc
+%%% Simple QuickCheck properties
+%%% @end
 -module(simple_eqc).
 
 -include_lib("eqc/include/eqc.hrl").
 
--compile(export_all).
+-export([prop_simple/0]).
 
+%% @doc This is a property to test something interesting
+%% @spec prop_simple() -> boolean()
+%% @end
+-spec prop_simple() -> boolean().
 prop_simple() ->
 	?FORALL({I, L}, {int(), list(int())},
 			not lists:member(I, lists:delete(I, L))).
