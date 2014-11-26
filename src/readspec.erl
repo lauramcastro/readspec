dc26d85519172a2f3f9f44d2c36fcb9f3ce50b48
Laura M. Castro <lcastro@udc.es>
2014-11-26 13:14:00 +0100

    Changed source indentation.

--- src/readspec.erl
+++ src/readspec.erl
@@ -19,150 +19,150 @@ suite(Module, Property) ->
 	suite(Module, Property, 50).
 
 suite(Module, Property, NumTests) ->
-	?DEBUG("Cover-compiling module: ~p~n", [Module]),
-	FeatureFile = erlang:atom_to_list(Module) ++ ".feature",
-	{ok, Module} = cover:compile(Module),
-	Suite = eqc_suite:coverage_based(Module,
-									 eqc:numtests(NumTests, erlang:apply(Module, Property, []))),
-	?DEBUG("Cucumberising set of test cases: ~p~n", [Suite]),
-	ok = file:write_file(FeatureFile,
-						 clean(erl_prettypr:format(cucumberise_suite(Module, Property, eqc_suite:cases(Suite)),
+    ?DEBUG("Cover-compiling module: ~p~n", [Module]),
+    FeatureFile = erlang:atom_to_list(Module) ++ ".feature",
+    {ok, Module} = cover:compile(Module),
+    Suite = eqc_suite:coverage_based(Module,
+				     eqc:numtests(NumTests, erlang:apply(Module, Property, []))),
+    ?DEBUG("Cucumberising set of test cases: ~p~n", [Suite]),
+    ok = file:write_file(FeatureFile,
+			 clean(erl_prettypr:format(cucumberise_suite(Module, Property, eqc_suite:cases(Suite)),
                                                    ?PRETTYPR_OPTIONS))).
 
 counterexample(Module, Property, [Counterexample]) ->
-	FeatureFile = erlang:atom_to_list(Property) ++ ".counterexample.feature",
-	?DEBUG("Generating counterexample file: ~p~n", [FeatureFile]),
-	Scenario = cucumberise_teststeps_aux(Module, Property, Counterexample, []),
-	?DEBUG("Reversing scenario: ~p~n", [readspec_inspect:falsify(Scenario)]),
-	ok = file:write_file(FeatureFile, clean(erl_prettypr:format(readspec_inspect:falsify(Scenario),
+    FeatureFile = erlang:atom_to_list(Property) ++ ".counterexample.feature",
+    ?DEBUG("Generating counterexample file: ~p~n", [FeatureFile]),
+    Scenario = cucumberise_teststeps_aux(Module, Property, Counterexample, []),
+    ?DEBUG("Reversing scenario: ~p~n", [readspec_inspect:falsify(Scenario)]),
+    ok = file:write_file(FeatureFile, clean(erl_prettypr:format(readspec_inspect:falsify(Scenario),
                                                                 ?PRETTYPR_OPTIONS))).
 
 %%% -------------------------------------------------------------- %%%
 
 cucumberise_suite(Module, Property, Suite) ->
-	FeatureName = erlang:atom_to_list(Module) -- "_eqc",
-	Scenarios = cucumberise_testcases(Module, Property, Suite, []),
-	erl_syntax:form_list([erl_syntax:string(?FEATURE ++ FeatureName),
-						  erl_syntax:comment(?EMPTY),
-						  erl_syntax:comment(2, [readspec_inspect:model_description(Module)])] ++
-							 [ erl_syntax:form_list([erl_syntax:comment(?EMPTY),
-													 erl_syntax:comment(?EMPTY),
-													 erl_syntax:string(?SCENARIO ++
-																		   readspec_inspect:property_description(Module, Property)),
-													 Scenario,
-													 erl_syntax:comment(?EMPTY)]) || Scenario <- Scenarios ] ).
+    FeatureName = erlang:atom_to_list(Module) -- "_eqc",
+    Scenarios = cucumberise_testcases(Module, Property, Suite, []),
+    erl_syntax:form_list([erl_syntax:string(?FEATURE ++ FeatureName),
+			  erl_syntax:comment(?EMPTY),
+			  erl_syntax:comment(2, [readspec_inspect:model_description(Module)])] ++
+			     [ erl_syntax:form_list([erl_syntax:comment(?EMPTY),
+						     erl_syntax:comment(?EMPTY),
+						     erl_syntax:string(?SCENARIO ++
+									   readspec_inspect:property_description(Module, Property)),
+						     Scenario,
+						     erl_syntax:comment(?EMPTY)]) || Scenario <- Scenarios ] ).
 
 
 cucumberise_testcases(_Module, _Property, [], CucumberisedTestCases) ->
-	lists:reverse(CucumberisedTestCases);
+    lists:reverse(CucumberisedTestCases);
 cucumberise_testcases(Module, Property, [TestCase | MoreTestCases], CucumberisedTestCases) ->
-	cucumberise_testcases(Module, Property, MoreTestCases, [cucumberise_teststeps(Module, Property, TestCase) | CucumberisedTestCases]).
+    cucumberise_testcases(Module, Property, MoreTestCases, [cucumberise_teststeps(Module, Property, TestCase) | CucumberisedTestCases]).
 
 
 cucumberise_teststeps(Module, Property, [TestCase]) ->
-	cucumberise_teststeps_aux(Module, Property, TestCase, []);
+    cucumberise_teststeps_aux(Module, Property, TestCase, []);
 cucumberise_teststeps(Module, Property, TestCase) ->
-	cucumberise_teststeps_aux(Module, Property, TestCase, []).
+    cucumberise_teststeps_aux(Module, Property, TestCase, []).
 
 cucumberise_teststeps_aux(Module, Property, [], CucumberisedTestSteps) ->
-	erl_syntax:form_list(cucumberise(Module, Property,
-									 {scenario, lists:reverse(CucumberisedTestSteps)}));
+    erl_syntax:form_list(cucumberise(Module, Property,
+				     {scenario, lists:reverse(CucumberisedTestSteps)}));
 % tests for QC properties
 cucumberise_teststeps_aux(Module, Property, Values, []) when is_tuple(Values) ->
-	erl_syntax:form_list(cucumberise(Module, Property,
-									 {scenario, Values}));
+    erl_syntax:form_list(cucumberise(Module, Property,
+				     {scenario, Values}));
 % test steps for QC state machines
 cucumberise_teststeps_aux(Module, Property, [{set,_,Call={call,_Module,_Function,_Args}} | MoreSteps], CucumberisedTestSteps) ->
-	cucumberise_teststeps_aux(Module, Property, MoreSteps, [Call | CucumberisedTestSteps]).
+    cucumberise_teststeps_aux(Module, Property, MoreSteps, [Call | CucumberisedTestSteps]).
 
 
 % we remove spureous cases such as {scenario, []}
 cucumberise(_Module, _Property, {scenario, []}) ->
-	erl_syntax:nil();
+    erl_syntax:nil();
 % cucumberise QC property scenario
 cucumberise(Module, Property, {scenario, Values}) when is_tuple(Values) ->
-	explain(Module, Property, Values, []);
+    explain(Module, Property, Values, []);
 % cucumberise QC state machine scenario
 cucumberise(Module, Property, {scenario, [Call={call,_,_,_} | MoreSteps]}) ->
-	explain(Module, Property, Call, MoreSteps).
+    explain(Module, Property, Call, MoreSteps).
 
 
 explain(_Module, _Property, {call,_,Function,Args}, MoreSteps) ->
-	"GIVEN " ++ enumerate_list([Args]) ++
-		" WHEN " ++ io_lib:fwrite("~p", [Function]) ++
-		            explain_also(MoreSteps) ++
-		" THEN ** insert property postcondition here ** ";
+    "GIVEN " ++ enumerate_list([Args]) ++
+	" WHEN " ++ io_lib:fwrite("~p", [Function]) ++
+	explain_also(MoreSteps) ++
+	" THEN ** insert property postcondition here ** ";
 explain(Module, Property, Values, []) ->
-	[erl_syntax:comment(?EMPTY),
-	 erl_syntax:string(?GIVEN),
-	 erl_syntax:form_list(enumerate_list(Values)),
-	 erl_syntax:string(?THEN  ++ 
-						   readspec_inspect:property_definition(Module, Property, Values)),
-	 erl_syntax:comment(?EMPTY)].
+    [erl_syntax:comment(?EMPTY),
+     erl_syntax:string(?GIVEN),
+     erl_syntax:form_list(enumerate_list(Values)),
+     erl_syntax:string(?THEN  ++ 
+			   readspec_inspect:property_definition(Module, Property, Values)),
+     erl_syntax:comment(?EMPTY)].
 
 
 explain_also([]) ->
-	"";
+    "";
 explain_also([{call,_Module,Function,_ArgsNotUsedRightNow} | MoreSteps]) ->
-	" AND " ++ io_lib:fwrite("~p", [Function]) ++
-		explain_also(MoreSteps).
+    " AND " ++ io_lib:fwrite("~p", [Function]) ++
+	explain_also(MoreSteps).
 
 
 % ----- ----- ----- ----- ----- -----  ----- ----- ----- ----- ----- %
 
 enumerate_list(Tuple) when is_tuple(Tuple) ->
-	enumerate_list(erlang:tuple_to_list(Tuple));
+    enumerate_list(erlang:tuple_to_list(Tuple));
 enumerate_list(List) when is_list(List) ->
-	L = [identify(X) || X <- List],
-	[_H | T] = lists:flatten(L),
-	T.
+    L = [identify(X) || X <- List],
+    [_H | T] = lists:flatten(L),
+    T.
 
 identify(X) ->
-	ASTofX = erl_syntax:abstract(X),
-	[erl_syntax:string(?AND), 
-	 erl_syntax:string(?OPERAND),
-	 erl_syntax:string(type_of(X)), %% TODO: check consistency with property definition
-	 ASTofX,
-	 erl_syntax:comment(?EMPTY)].
+    ASTofX = erl_syntax:abstract(X),
+    [erl_syntax:string(?AND), 
+     erl_syntax:string(?OPERAND),
+     erl_syntax:string(type_of(X)), %% TODO: check consistency with property definition
+     ASTofX,
+     erl_syntax:comment(?EMPTY)].
 
 type_of([]) ->
-	?LIST;
+    ?LIST;
 type_of(X) when is_integer(X) ->
-	?INTEGER;
+    ?INTEGER;
 type_of(X) when is_boolean(X) ->
-	?BOOLEAN;
+    ?BOOLEAN;
 type_of(X) when is_atom(X) ->
-	?ATOM;
+    ?ATOM;
 type_of(X) when is_list(X) ->
-	case is_string(X) of
-		true  -> ?STRING;
-		false -> ?LIST
-	end;
+    case is_string(X) of
+	true  -> ?STRING;
+	false -> ?LIST
+    end;
 type_of(X) when is_tuple(X) ->
-	?TUPLE;
+    ?TUPLE;
 type_of(_) ->
-	?UNKNOWN.
+    ?UNKNOWN.
 
 is_string(X) ->
-	is_list(X) andalso lists:all(fun(C) -> is_char(C) end, X).
+    is_list(X) andalso lists:all(fun(C) -> is_char(C) end, X).
 
 is_char(C) when is_integer(C) ->
-	((32 =< C) andalso (C =< 126)) orelse ((161 =< C) andalso (C =< 255)).
+    ((32 =< C) andalso (C =< 126)) orelse ((161 =< C) andalso (C =< 255)).
 
 
 clean(StringStream) ->
-	trim_lines(lists:filter(fun($") -> false;
-							   ($%) -> false;
-							   (_C) -> true   end,
-							StringStream)).
+    trim_lines(lists:filter(fun($") -> false;
+			       ($%) -> false;
+			       (_C) -> true   end,
+			    StringStream)).
 
 trim_lines([]) ->
-	[];
+    [];
 trim_lines([$\n, $\n, $\n, $\n | T]) ->
-	[$\n, $\n | trim_lines(T)];
+    [$\n, $\n | trim_lines(T)];
 trim_lines([$\n, $\n, $n | T]) ->
-	[$\n | trim_lines(T)];
+    [$\n | trim_lines(T)];
 trim_lines([$\n, $\n | T]) ->
-	trim_lines(T);
+    trim_lines(T);
 trim_lines([H|T]) ->
-	[H | trim_lines(T)].
+    [H | trim_lines(T)].
--- src/readspec_inspect.erl
+++ src/readspec_inspect.erl
@@ -21,32 +21,32 @@
 %% @end
 -spec model_description(ModelModule :: atom()) -> ModuleDescription :: string().
 model_description(ModelModule) ->
-	extract_module_description(ModelModule).
+    extract_module_description(ModelModule).
 
 %% @doc Extracts the property description from the edoc comments on the source code
 %% @end
 -spec property_description(ModelModule :: atom(),
                            PropertyName :: atom()) -> PropertyDescription :: string().
 property_description(ModelModule, PropertyName) ->
-	extract_function_description(ModelModule, PropertyName).
+    extract_function_description(ModelModule, PropertyName).
 
 %% @doc Extracts the body of a property as a string from the source code
 %% @end
 -spec property_definition(ModelModule :: atom(),
-						  PropertyName :: atom(),
-						  Values :: tuple()) -> PropertyBody :: string().
+			  PropertyName :: atom(),
+			  Values :: tuple()) -> PropertyBody :: string().
 property_definition(ModelModule, PropertyName, Values) ->
-	property_definition(ModelModule, PropertyName, [], Values).
+    property_definition(ModelModule, PropertyName, [], Values).
 
 -spec property_definition(ModelModule :: atom(),
-						  PropertyName :: atom(),
-						  Args :: term(),
-						  Values :: tuple()) -> PropertyBody :: string().
+			  PropertyName :: atom(),
+			  Args :: term(),
+			  Values :: tuple()) -> PropertyBody :: string().
 property_definition(ModelModule, PropertyName, Arguments, Values) ->
-	FullModelModuleName = list_to_atom(atom_to_list(ModelModule) ++ ".erl"),
-	ValuesAsAtoms = [to_atom(Value) || Value <- tuple_to_list(Values)],
-	[Exp] = see:scan_func_str_args(FullModelModuleName, PropertyName, Arguments),
-	extract_property_definition(Exp, ValuesAsAtoms).
+    FullModelModuleName = list_to_atom(atom_to_list(ModelModule) ++ ".erl"),
+    ValuesAsAtoms = [to_atom(Value) || Value <- tuple_to_list(Values)],
+    [Exp] = see:scan_func_str_args(FullModelModuleName, PropertyName, Arguments),
+    extract_property_definition(Exp, ValuesAsAtoms).
 
 
 %% @doc Reverses the truth value of a given property (for counterexample explanation).
@@ -54,116 +54,116 @@ property_definition(ModelModule, PropertyName, Arguments, Values) ->
 -type syntaxTree() :: any(). % as used in the OTP syntax tools (erl_syntax)
 -spec falsify(Scenario :: syntaxTree()) -> syntaxTree().
 falsify(Scenario) ->
-	falsify_aux(Scenario).
+    falsify_aux(Scenario).
 
 %%% -------------------------------------------------------------- %%%
 
 extract_module_description(ModelModule) ->
-	XML = get_xml_version(ModelModule),
-	module = XML#xmlElement.name,
-	Descriptions = lists:flatten([ Element#xmlElement.content || Element <- XML#xmlElement.content,
-																 Element#xmlElement.name == description ]),
-	[FullDescription] = lists:flatten([ Element#xmlElement.content || Element <- Descriptions,
-																	  Element#xmlElement.name == fullDescription ]),
-	FullDescription#xmlText.value.
+    XML = get_xml_version(ModelModule),
+    module = XML#xmlElement.name,
+    Descriptions = lists:flatten([ Element#xmlElement.content || Element <- XML#xmlElement.content,
+								 Element#xmlElement.name == description ]),
+    [FullDescription] = lists:flatten([ Element#xmlElement.content || Element <- Descriptions,
+								      Element#xmlElement.name == fullDescription ]),
+    FullDescription#xmlText.value.
 
 
 extract_function_description(ModelModule, Function) ->
-	FunctionName = erlang:atom_to_list(Function),
-	XML = get_xml_version(ModelModule),
-	module = XML#xmlElement.name,
-	Functions = lists:flatten([ Element#xmlElement.content || Element <- XML#xmlElement.content,
-															  Element#xmlElement.name == functions ]),
-	[FunctionDescription] = lists:filter(fun(Element) when is_record(Element, xmlElement) -> 
-												 [] =/= [ Element#xmlElement.content || Attribute <- Element#xmlElement.attributes,
-																						Attribute#xmlAttribute.name == name,
-																						Attribute#xmlAttribute.value == FunctionName ]
-										 end, Functions),
-	function = FunctionDescription#xmlElement.name,
-	Descriptions = lists:flatten([ Element#xmlElement.content || Element <- FunctionDescription#xmlElement.content,
-																 Element#xmlElement.name == description ]),
-	[FullDescription] = lists:flatten([ Element#xmlElement.content || Element <- Descriptions,
-																	  Element#xmlElement.name == fullDescription ]),
-	FullDescription#xmlText.value.
+    FunctionName = erlang:atom_to_list(Function),
+    XML = get_xml_version(ModelModule),
+    module = XML#xmlElement.name,
+    Functions = lists:flatten([ Element#xmlElement.content || Element <- XML#xmlElement.content,
+							      Element#xmlElement.name == functions ]),
+    [FunctionDescription] = lists:filter(fun(Element) when is_record(Element, xmlElement) -> 
+						 [] =/= [ Element#xmlElement.content || Attribute <- Element#xmlElement.attributes,
+											Attribute#xmlAttribute.name == name,
+											Attribute#xmlAttribute.value == FunctionName ]
+					 end, Functions),
+    function = FunctionDescription#xmlElement.name,
+    Descriptions = lists:flatten([ Element#xmlElement.content || Element <- FunctionDescription#xmlElement.content,
+								 Element#xmlElement.name == description ]),
+    [FullDescription] = lists:flatten([ Element#xmlElement.content || Element <- Descriptions,
+								      Element#xmlElement.name == fullDescription ]),
+    FullDescription#xmlText.value.
 
 get_xml_version(Module) ->
-	FileName = erlang:atom_to_list(Module) ++ ".erl",
-	{Module, XML} = edoc_extract:source(FileName, edoc_lib:get_doc_env(FileName), []),
-	XML.
+    FileName = erlang:atom_to_list(Module) ++ ".erl",
+    {Module, XML} = edoc_extract:source(FileName, edoc_lib:get_doc_env(FileName), []),
+    XML.
 
 % ----- ----- ----- ----- ----- -----  ----- ----- ----- ----- ----- %
 
 extract_property_definition(Exp, Values) when is_record(Exp, exp_iface) ->
-	[AppDef] = Exp#exp_iface.var_defs,
-	extract_property_definition_aux(AppDef, Values).
+    [AppDef] = Exp#exp_iface.var_defs,
+    extract_property_definition_aux(AppDef, Values).
 
 extract_property_definition_aux(App, Values) when is_record(App, apply) ->
-	[FunDef] = lists:flatten([ Clauses || {'fun',_,{clauses,Clauses}} <- App#apply.arg_list]),
-	[FunBody] = erl_syntax:clause_body(FunDef),
-	?DEBUG("Pretty-printing: ~p~n", [{FunBody,replace_values(FunBody, Values)}]),
-	case replace_values(FunBody, Values) of
-		{op,_,'not',Property} ->
-			erl_prettypr:format(Property) ++ ?ISFALSE;
-		Property ->
-			erl_prettypr:format(Property)
-	end.
+    [FunDef] = lists:flatten([ Clauses || {'fun',_,{clauses,Clauses}} <- App#apply.arg_list]),
+    [FunBody] = erl_syntax:clause_body(FunDef),
+    ?DEBUG("Pretty-printing: ~p~n", [{FunBody,replace_values(FunBody, Values)}]),
+    case replace_values(FunBody, Values) of
+	{op,_,'not',Property} ->
+	    erl_prettypr:format(Property) ++ ?ISFALSE;
+	Property ->
+	    erl_prettypr:format(Property)
+    end.
 
 replace_values(Exp, Values) ->
-	{NewExp,_NotBindedValues,_BindedValues} = transverse_exp(Exp, Values, []),
-	NewExp.
+    {NewExp,_NotBindedValues,_BindedValues} = transverse_exp(Exp, Values, []),
+    NewExp.
 
 transverse_exp({var,N,Name}, [Value|MoreValues], UsedValues) when is_atom(Name) ->
-	case lists:keyfind(Name, 1, UsedValues) of
-		false ->
-			{{var,N,Value}, MoreValues, [{Name,Value}|UsedValues]};
-		{Name,UsedValue} ->
-			{{var,N,UsedValue}, [Value|MoreValues], UsedValues}
-	end;
+    case lists:keyfind(Name, 1, UsedValues) of
+	false ->
+	    {{var,N,Value}, MoreValues, [{Name,Value}|UsedValues]};
+	{Name,UsedValue} ->
+	    {{var,N,UsedValue}, [Value|MoreValues], UsedValues}
+    end;
 transverse_exp(Exp, Values, UsedValues) when is_tuple(Exp) ->
-	ExpList = tuple_to_list(Exp),
-	{NewExpList, LessValues, MoreUsedValues} = transverse_exp(ExpList, Values, UsedValues),
-	{list_to_tuple(NewExpList), LessValues, MoreUsedValues};
+    ExpList = tuple_to_list(Exp),
+    {NewExpList, LessValues, MoreUsedValues} = transverse_exp(ExpList, Values, UsedValues),
+    {list_to_tuple(NewExpList), LessValues, MoreUsedValues};
 transverse_exp(Exp, Values, UsedValues) when is_list(Exp) ->
-	lists:foldl(fun(Member, {RExp, Vs, UVs}) ->
-						case transverse_exp(Member, Vs, UVs) of
-							{{var,N,Value}, MoreValues, MoreUsedValues} ->
-								{RExp++[{var,N,Value}], MoreValues, MoreUsedValues};
-							{Other, MoreVs, MoreUsedVs} ->
-								{RExp++[Other], MoreVs, MoreUsedVs}
-						end
-				end, {[], Values, UsedValues}, Exp);
+    lists:foldl(fun(Member, {RExp, Vs, UVs}) ->
+			case transverse_exp(Member, Vs, UVs) of
+			    {{var,N,Value}, MoreValues, MoreUsedValues} ->
+				{RExp++[{var,N,Value}], MoreValues, MoreUsedValues};
+			    {Other, MoreVs, MoreUsedVs} ->
+				{RExp++[Other], MoreVs, MoreUsedVs}
+			end
+		end, {[], Values, UsedValues}, Exp);
 transverse_exp(Exp, Values, UsedValues) ->
-	{Exp, Values, UsedValues}.
+    {Exp, Values, UsedValues}.
 
 
 falsify_aux({tree,form_list,Attributes,List}) ->
-	{tree,form_list,Attributes,falsify(List)};
+    {tree,form_list,Attributes,falsify(List)};
 falsify_aux(Term = {tree,string,Attributes,Text}) ->
-	[First | Words] = string:tokens(Text, " "),
-	case string:equal(First, string:strip(?THEN)) of
+    [First | Words] = string:tokens(Text, " "),
+    case string:equal(First, string:strip(?THEN)) of
+	true ->
+	    [Last, PrevToLast | RestOfWords] = lists:reverse(Words),
+	    case string:equal(PrevToLast ++ " " ++ Last, string:strip(?ISFALSE)) of
 		true ->
-			[Last, PrevToLast | RestOfWords] = lists:reverse(Words),
-			case string:equal(PrevToLast ++ " " ++ Last, string:strip(?ISFALSE)) of
-				true ->
-					{tree,string,Attributes,lists:flatten([[First, 32], lists:reverse(RestOfWords), ?ISTRUE])};
-				false ->
-					{tree,string,Attributes,lists:flatten([[First, 32], lists:reverse(RestOfWords), ?ISFALSE])}
-			end;
+		    {tree,string,Attributes,lists:flatten([[First, 32], lists:reverse(RestOfWords), ?ISTRUE])};
 		false ->
-			Term
-	end;
+		    {tree,string,Attributes,lists:flatten([[First, 32], lists:reverse(RestOfWords), ?ISFALSE])}
+	    end;
+	false ->
+	    Term
+    end;
 falsify_aux(TermList) when is_list(TermList) ->	
-	[falsify(Term) || Term <- TermList];
+    [falsify(Term) || Term <- TermList];
 falsify_aux(Other) ->	
-	Other.
+    Other.
 
 to_atom(Term) when is_integer(Term) ->
-	list_to_atom(to_list(Term));
+    list_to_atom(to_list(Term));
 to_atom([]) ->
-	'[]';
+    '[]';
 to_atom(Term) when is_list(Term) ->
-	List = lists:foldl(fun(T, AccIn) -> AccIn ++ to_list(T) ++ ","  end, "[", Term),
-	list_to_atom(string:substr(List, 1, length(List)-1) ++ "]").
+    List = lists:foldl(fun(T, AccIn) -> AccIn ++ to_list(T) ++ ","  end, "[", Term),
+    list_to_atom(string:substr(List, 1, length(List)-1) ++ "]").
 
 to_list(Term) when is_integer(Term) ->
-	integer_to_list(Term).
+    integer_to_list(Term).
